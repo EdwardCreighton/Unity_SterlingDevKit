4d3dea9468752de9ad31bfd023c4d0319a21633e
Edward Creighton <eduard.chekukha@mail.ru>
2023-05-19 16:32:31 +0300

    Added EditorUpdateWindow instead of MonoBehaviour ExecuteInEditModeManager Refactor

---
+++ Assets/_General/Editor Window Extensions/Editor/EditorWindowWrapper.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace SterlingTools
+{
+	public class EditorWindowWrapper : EditorWindow
+	{
+		protected VisualElement GetSpace(int size = 1)
+		{
+			string temp = String.Empty;
+
+			size = Mathf.Max(size, 1);
+
+			for (int i = 0; i < size; i++)
+			{
+				temp += "\n ";
+			}
+			
+			return new Label(temp);
+		}
+		
+		protected Button GetButton(string buttonTitle, Action action)
+		{
+			Button button = new Button(action)
+			{
+				text = buttonTitle
+			};
+
+			return button;
+		}
+	}
+}
